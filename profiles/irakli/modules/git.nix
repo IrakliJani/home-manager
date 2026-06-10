c9b800b994aeb0f9ed7bda7b03d6d17c5ee2057f
Irakli Janiashvili <irakli.janiashvili@gmail.com>
2026-06-10 21:19:34 +0400

    fix(git): verify SSH signatures

--- profiles/irakli/modules/git.nix
+++ profiles/irakli/modules/git.nix
@@ -1,18 +1,29 @@
-{ ... }:
+{ config, ... }:
 
+let
+  userName = "Irakli Janiashvili";
+  userEmail = "irakli.janiashvili@gmail.com";
+  signingKey = "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIA/48Asjhf6j0n2pUZjb2NX9klFv/Z5VBpUq+lFUkSPX";
+in
 {
+  xdg.configFile."git/allowed_signers".text = ''
+    ${userEmail} ${signingKey}
+  '';
+
   programs.git = {
     signing = {
-      key = "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIA/48Asjhf6j0n2pUZjb2NX9klFv/Z5VBpUq+lFUkSPX";
+      key = signingKey;
       signByDefault = true;
       format = "ssh";
       # signer set in modules/platform/darwin.nix
     };
 
     settings = {
+      gpg.ssh.allowedSignersFile = "${config.xdg.configHome}/git/allowed_signers";
+
       user = {
-        name = "Irakli Janiashvili";
-        email = "irakli.janiashvili@gmail.com";
+        name = userName;
+        email = userEmail;
       };
     };
   };
