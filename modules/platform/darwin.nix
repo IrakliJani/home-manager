f60b1bbc3ff2dc5113540763fbe093e6c61e7a02
Irakli Janiashvili <irakli.janiashvili@gmail.com>
2026-08-07 15:20:51 +0400

    feat(nix): prune Home Manager generations

--- modules/platform/darwin.nix
+++ modules/platform/darwin.nix
@@ -1,7 +1,18 @@
-{ ... }:
+{ lib, pkgs, ... }:
 
 {
   home.homeDirectory = "/Users/irakli";
+
+  nix.gc = {
+    automatic = true;
+    dates = "weekly";
+  };
+
+  launchd.agents.nix-gc.config.ProgramArguments = lib.mkForce [
+    "${pkgs.nix}/bin/nix-collect-garbage"
+    "--delete-older-than"
+    "14d"
+  ];
 
   programs.git.signing.signer = "/Applications/1Password.app/Contents/MacOS/op-ssh-sign";
 }
