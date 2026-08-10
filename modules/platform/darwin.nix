addb201b9f1ae4343647af6a7e081e96b8b5030a
Irakli Janiashvili <irakli.janiashvili@gmail.com>
2026-08-10 23:19:53 +0300

    warn on missing Ghostty terminfo

--- modules/platform/darwin.nix
+++ modules/platform/darwin.nix
@@ -1,17 +1,25 @@
 { lib, pkgs, ... }:
 
 let
+  ghosttyTerminfoSource = ./ghostty.terminfo;
+  ghosttyTerminfoSourceExists = builtins.pathExists ghosttyTerminfoSource;
   ghosttyTerminfo = pkgs.runCommand "ghostty-terminfo" { } ''
     mkdir -p "$out/share/terminfo"
-    ${pkgs.ncurses}/bin/tic -x -o "$out/share/terminfo" ${./ghostty.terminfo}
+    ${pkgs.ncurses}/bin/tic -x -o "$out/share/terminfo" ${ghosttyTerminfoSource}
     test -f "$out/share/terminfo/78/xterm-ghostty"
   '';
 in
 {
   home.homeDirectory = "/Users/irakli";
 
-  home.file.".terminfo/78/xterm-ghostty".source =
-    "${ghosttyTerminfo}/share/terminfo/78/xterm-ghostty";
+  warnings = lib.optional (!ghosttyTerminfoSourceExists) ''
+    Ghostty terminfo source is missing at modules/platform/ghostty.terminfo;
+    xterm-ghostty will not be installed.
+  '';
+
+  home.file = lib.optionalAttrs ghosttyTerminfoSourceExists {
+    ".terminfo/78/xterm-ghostty".source = "${ghosttyTerminfo}/share/terminfo/78/xterm-ghostty";
+  };
 
   nix.gc = {
     automatic = true;
